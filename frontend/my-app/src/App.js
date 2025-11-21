dfe3eaedd562227a3e1944a94bf58ba1025b4f62
mllwrl <kiemie71@yandex.ru>
2025-11-21 21:32:22 +0400

    React to NotYourProfile

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Auth from './Components/Auth';
 import Main from './Components/Main';
 import Registration from './Components/Registration';
+import NotYourProfile from './Components/NotYourProfile';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Main/>}></Route>
           <Route path="/auth" element={<Auth/>}></Route>
           <Route path="/registration" element={<Registration/>}></Route>
+          <Route path="/handler" element={<NotYourProfile/>}></Route>
         </Routes>
         </BrowserRouter>
     </div>
