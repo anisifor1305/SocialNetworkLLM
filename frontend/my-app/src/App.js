544f12ab8f3545e63663ab1c2b95b5d122334937
Geyorgy <user@MacBook-Air-Geyorgy.local>
2025-11-22 10:10:14 +0400

    sosite

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -8,13 +8,10 @@ import Registration from './Components/Registration';
 import ProtectedRoute from './Components/ProtectedRoute'
 import { AuthProvider } from './Contexts/AuthContext';
 import NotYourProfile from './Components/NotYourProfile'
-<<<<<<< HEAD
 import NewPost from './Components/NewPost';
-=======
 import MyProfile from './Components/MyProfile'
 import Forums from './Components/Forums';
 import CreateForum from './Components/CreateForum';
->>>>>>> 00d07f1979f36db3cc94d4c9cc1fe4dd0333a899
 function App() {
   return (
     <div className="App">
@@ -26,15 +23,12 @@ function App() {
           <Route path="/auth" element={<Auth/>}></Route>
           <Route path="/registration" element={<Registration/>}></Route>
           <Route path="/" element={<ProtectedRoute><Main /></ProtectedRoute>} />
-<<<<<<< HEAD
           <Route path="/handler" element={<ProtectedRoute><NotYourProfile /></ProtectedRoute>} />
           <Route path="/newpost" element={<ProtectedRoute><NewPost /></ProtectedRoute>} />
-=======
           <Route path="/:handle" element={<ProtectedRoute><NotYourProfile /></ProtectedRoute>} />
           <Route path="/myprofile" element={<ProtectedRoute><MyProfile /></ProtectedRoute>} />
           <Route path="/forums" element={<ProtectedRoute><Forums /></ProtectedRoute>} />
           <Route path="/createforum" element={<ProtectedRoute><CreateForum /></ProtectedRoute>} />
->>>>>>> 00d07f1979f36db3cc94d4c9cc1fe4dd0333a899
         </Routes>
         </AuthProvider>
         </BrowserRouter>
